f15b6c8c955ad92c9f909ef85a3e482eed045764
rkg2359 <rkg2359@gmail.com>
2017-11-18 11:06:19 -0500

    Initial Setup with sampleTest

---
+++ StringWrap/StringWrapper.Library.Tests/StringWrapperTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringWrapper.Library.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void sampleTest()
+        {
+            Assert.Pass();
+        }
+
+    }
+}
